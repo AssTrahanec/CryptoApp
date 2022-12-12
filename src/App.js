e3eba6bb44e7f4fdd2233c65abb94bc5c31593f4
AssTrahanec <max197402@gmail.com>
2022-12-12 19:39:48 +0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import {Layout,Typography, Space} from 'antd';
+
+import {Navbar, Homepage, Cryptocurrencies, CryptoDetails} from './components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+
+        <div className="app">
+            <div className = "navbar">
+                <Navbar />
+            </div>
+            <div className="main">
+                    <div className="routes">
+                        <Switch>
+                            <Route exact path = "/">
+                                <Homepage />
+                            </Route>
+                            <Route exact path = "/cryptocurrencies">
+                                <Cryptocurrencies />
+                            </Route>
+                            <Route exact path = "/crypto/:coinId">
+                                <CryptoDetails />
+                            </Route>
+                        </Switch>
+                    </div>
+            </div>
+            <div className="footer">
+
+            </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,47 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '022ba2dd8emshfcaada0b713deabp115fb5jsn4e312c0db87a',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) =>({url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+
+        getCryptoHistory: builder.query({
+            query: ({coinId, timePeriod}) => createRequest(`coin/${coinId}/history?timePeriod=${timePeriod}`),
+        }),
+
+        getExchanges: builder.query({
+            query: () => createRequest('/exchanges'),
+        }),
+    }),
+});
+
+export const {
+    useGetCryptosQuery,
+    useGetCryptoDetailsQuery,
+    useGetExchangesQuery,
+    useGetCryptoHistoryQuery,
+} = cryptoApi;
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     headers: {
+//         'X-RapidAPI-Key': '022ba2dd8emshfcaada0b713deabp115fb5jsn4e312c0db87a',
+//         'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+// };
